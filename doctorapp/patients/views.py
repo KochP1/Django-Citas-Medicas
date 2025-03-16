6b24888f184120582d7b63844f3e6907c02518f5
root <juanandreskoch@gmail.com>
2025-03-16 03:32:41 -0400

    Generics

--- doctorapp/patients/views.py
+++ doctorapp/patients/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 
@@ -13,47 +14,12 @@ from rest_framework.views import APIView
 # GET /api/patients/<pk> => Detalle
 # PUT /api/patients => Modificar
 
-class ListPatients(APIView):
+class ListPatients(ListAPIView, CreateAPIView):
     allowed_methods = ['GET', 'POST', 'HEAD', 'OPTIONS']
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
 
-    def get(self, request):
-        patients = Patient.objects.all()
-        serializer = PatientSerializer(patients, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = PatientSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status= status.HTTP_201_CREATED)
-
-class DetailPatients(APIView):
+class DetailPatients(RetrieveUpdateDestroyAPIView):
     allowed_methods = ['GET', 'PUT', 'DELETE']
-
-    def get(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = PatientSerializer(patient)
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = PatientSerializer(patient, data = request.data)
-        serializer.is_valid()
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    def delete(self, request, pk):
-        try:
-            patient = Patient.objects.get(id=pk)
-        except Patient.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        patient.delete()
-        return Response(status=status.HTTP_200_OK)
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
