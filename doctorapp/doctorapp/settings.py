d775b3a1e1d59a148523596886153781664b0e98
root <juanandreskoch@gmail.com>
2025-03-16 20:00:16 -0400

    Throttling

--- doctorapp/doctorapp/settings.py
+++ doctorapp/doctorapp/settings.py
@@ -131,6 +131,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
                 'rest_framework.authentication.BasicAuthentication',
                 'rest_framework.authentication.SessionAuthentication',
-                )
+                ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '1000/minute'
+    }
 
 }
